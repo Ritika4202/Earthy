9032c7ba8336e85ac976bef14daf6b1929011aeb
RITIKA GORANTLA <ritika4202@gmail.com>
2023-02-09 02:09:46 +0530

    updates

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/earthly-ritika'));
+app.use(express.static(__dirname + '/dist/helloworld'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/earthly-ritika/index.html'));
+res.sendFile(path.join(__dirname+'/dist/helloworld/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
